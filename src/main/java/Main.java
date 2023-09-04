006f5e711fe0a15dfe2d599266f1cd63499103d2
ConfuzzedCat <73724128+ConfuzzedCat@users.noreply.github.com>
2023-09-04 10:18:39 +0200

    initial Packages created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import config.HibernateConfig;
 import jakarta.persistence.EntityManagerFactory;
 
 public class Main {
@@ -5,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         HibernateConfig.addAnnotatedClasses();
         emf = HibernateConfig.getEntityManagerFactoryConfig("hobbydb");
+
     }
 }
