3a4061faf726f64488cc6535f95d746f3be2d6ab
DenSonmez <113019821+DenSonmez@users.noreply.github.com>
2023-09-04 11:22:56 +0200

    persondao

--- src/main/java/dao/PersonDAOImpl.java
+++ src/main/java/dao/PersonDAOImpl.java
@@ -1,4 +1,120 @@
 package dao;
 
-public class PersonDAOImpl {
+import dao.interfaces.IPersonDAO;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
+import model.Hobby;
+import model.Person;
+
+import java.util.List;
+
+public class PersonDAOImpl implements IPersonDAO {
+
+    private static EntityManagerFactory emf;
+    private static IPersonDAO instance;
+
+    public static IPersonDAO getInstance(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new IPersonDAO() {
+
+
+
+                @Override
+                public void createPerson(Person person) {
+                 try(EntityManager em = emf.createEntityManager()){
+                     em.getTransaction().begin();
+                     em.persist(person);
+                     em.getTransaction().commit();
+                 }
+                }
+
+                @Override
+                public Person updatePerson(Person person) {
+                    try(EntityManager em = emf.createEntityManager()){
+                        em.getTransaction().begin();
+                        em.merge(person);
+                        em.getTransaction().commit();
+                    }
+                    return person;                }
+
+                @Override
+                public void deletePerson(Person person) {
+                    try(EntityManager em = emf.createEntityManager()){
+                        em.getTransaction().begin();
+                        em.remove(person);
+                        em.getTransaction().commit();
+                    }
+                }
+
+                @Override
+                public Person findById(Integer personId) {
+                    return null;
+                }
+
+                @Override
+                public Person findAddressPersonById(Integer personId) {
+                    try (EntityManager em = emf.createEntityManager()) {
+                        return em.find(Person.class,personId);
+
+                    }
+                }
+
+                @Override
+                public List<Person> findPerson() {
+
+                    try (EntityManager em = emf.createEntityManager()) {
+                    TypedQuery<Person> typedQuery = em.createNamedQuery("", Person.class);
+                        return  typedQuery.getResultList();
+              
+                    }
+
+                }
+
+                @Override
+                public List<Person> findPersonByHobby(Hobby hobby) {
+                    return null;
+                }
+            };
+        }
+        return instance;
+    }
+
+
+
+    @Override
+    public Person createPerson(Person person) {
+        return null;
+    }
+
+    @Override
+    public Person updatePerson(Person person) {
+        return null;
+    }
+
+    @Override
+    public Person deletePerson(Person person) {
+        return null;
+    }
+
+    @Override
+    public Person findById(Integer personId) {
+        return null;
+    }
+
+    @Override
+    public Person findAddressPersonById(Integer personId) {
+        return null;
+    }
+
+    @Override
+    public List<Person> findPerson() {
+        return null;
+    }
+
+    @Override
+    public List<Person> findPersonByHobby(Hobby hobby) {
+        return null;
+    }
 }
--- src/main/java/dao/interfaces/IPersonDAO.java
+++ src/main/java/dao/interfaces/IPersonDAO.java
@@ -1,4 +1,27 @@
 package dao.interfaces;
 
+import model.Hobby;
+import model.Person;
+import org.hibernate.dialect.lock.PessimisticEntityLockException;
+
+import java.util.List;
+
 public interface IPersonDAO {
+
+
+    void createPerson(Person person);
+
+    Person updatePerson(Person person);
+
+    void deletePerson(Person person);
+
+    Person findById(Integer personId);
+
+    Person findAddressPersonById(Integer personId);
+
+    List<Person> findPerson();
+
+    List<Person> findPersonByHobby(Hobby hobby);
+
+
 }
