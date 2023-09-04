630aa530b03d1cecf53ec23859aae97dc94cd2de
RasmanHuhu <112378651+RasmanHuhu@users.noreply.github.com>
2023-09-04 10:51:01 +0200

    person

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -1,5 +1,6 @@
 package model;
 
+<<<<<<< Updated upstream
 
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -12,15 +13,30 @@ import lombok.ToString;
 
 @Table(name = "person")
 @Entity
+=======
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+
+>>>>>>> Stashed changes
 public class Person {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false, unique = true)
+<<<<<<< Updated upstream
     private String id;
 
     @Column(name = "first_name", nullable = false)
     private String firstname;
+=======
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+>>>>>>> Stashed changes
 
     @Column(name = "surname", nullable = false)
     private String surname;
@@ -28,6 +44,7 @@ public class Person {
     @Column(name = "age", nullable = false)
     private int age;
 
+<<<<<<< Updated upstream
     @Column(name = "phone_number", nullable = false)
     private int phoneNumber;
 
@@ -35,6 +52,20 @@ public class Person {
         this.firstname = firstname;
         this.surname = surname;
         this.age = age;
+=======
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "phone_number", unique = true, nullable = false)
+    private String phoneNumber;
+
+    @Builder
+    public Person(String name, String surname, int age, String email, String phoneNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.email = email;
+>>>>>>> Stashed changes
         this.phoneNumber = phoneNumber;
     }
 }
