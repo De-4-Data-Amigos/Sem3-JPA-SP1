e1c7417d267f59d5b701a9f37acf065e338e69c6
RasmanHuhu <112378651+RasmanHuhu@users.noreply.github.com>
2023-09-04 11:00:10 +0200

    person + hobby

---
+++ src/main/java/model/Hobby.java
@@ -0,0 +1,27 @@
+package model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Hobby {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private String id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "wiki_link", nullable = false)
+    private String wikiLink;
+
+    @Column(name = "category", nullable = false)
+    private String category;
+
+    @Column(name = "type", nullable = false)
+    private String type;
+
+}
--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -1,8 +1,7 @@
 package model;
 
-<<<<<<< Updated upstream
-
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -10,33 +9,17 @@ import lombok.ToString;
 @Getter
 @NoArgsConstructor
 @ToString
-
 @Table(name = "person")
 @Entity
-=======
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Builder;
-
->>>>>>> Stashed changes
 public class Person {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false, unique = true)
-<<<<<<< Updated upstream
     private String id;
 
     @Column(name = "first_name", nullable = false)
-    private String firstname;
-=======
-    private int id;
-
-    @Column(name = "name", nullable = false)
-    private String name;
->>>>>>> Stashed changes
+    private String firstName;
 
     @Column(name = "surname", nullable = false)
     private String surname;
@@ -44,28 +27,17 @@ public class Person {
     @Column(name = "age", nullable = false)
     private int age;
 
-<<<<<<< Updated upstream
+    private String email;
+
     @Column(name = "phone_number", nullable = false)
     private int phoneNumber;
 
-    public Person(String firstname, String surname, int age, int phoneNumber) {
-        this.firstname = firstname;
-        this.surname = surname;
-        this.age = age;
-=======
-    @Column(name = "email", unique = true, nullable = false)
-    private String email;
-
-    @Column(name = "phone_number", unique = true, nullable = false)
-    private String phoneNumber;
-
     @Builder
-    public Person(String name, String surname, int age, String email, String phoneNumber) {
-        this.name = name;
+    public Person(String name, String surname, int age, String email, int phoneNumber) {
+        this.firstName = firstName;
         this.surname = surname;
         this.age = age;
         this.email = email;
->>>>>>> Stashed changes
         this.phoneNumber = phoneNumber;
     }
 }
