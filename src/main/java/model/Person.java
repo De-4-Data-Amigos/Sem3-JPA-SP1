12986a27a2fa541966a8f577889b026d0e5c7a0d
RasmanHuhu <112378651+RasmanHuhu@users.noreply.github.com>
2023-09-04 10:38:54 +0200

    person

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -1,4 +1,40 @@
 package model;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@NoArgsConstructor
+@ToString
+
+@Table(name = "person")
+@Entity
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private String id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstname;
+
+    @Column(name = "surname", nullable = false)
+    private String surname;
+
+    @Column(name = "age", nullable = false)
+    private int age;
+
+    @Column(name = "phone_number", nullable = false)
+    private int phoneNumber;
+
+    public Person(String firstname, String surname, int age, int phoneNumber) {
+        this.firstname = firstname;
+        this.surname = surname;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+    }
 }
