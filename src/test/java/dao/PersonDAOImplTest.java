edfc068ae901eed44d5f99f1766ee24c6d32435f
RasmanHuhu <112378651+RasmanHuhu@users.noreply.github.com>
2023-09-04 12:46:32 +0200

    oprettet tests

---
+++ src/test/java/dao/PersonDAOImplTest.java
@@ -0,0 +1,36 @@
+package dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonDAOImplTest {
+
+    @Test
+    void createPerson() {
+    }
+
+    @Test
+    void updatePerson() {
+    }
+
+    @Test
+    void deletePerson() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findCityPersonById() {
+    }
+
+    @Test
+    void findPerson() {
+    }
+
+    @Test
+    void findPersonByHobby() {
+    }
+}
