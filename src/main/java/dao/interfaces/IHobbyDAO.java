ae6ed99577da6e16a592ed7e779e0ab89b00bb91
RasmanHuhu <112378651+RasmanHuhu@users.noreply.github.com>
2023-09-04 11:48:00 +0200

    HobbyDAO begyndt

--- src/main/java/dao/interfaces/IHobbyDAO.java
+++ src/main/java/dao/interfaces/IHobbyDAO.java
@@ -1,4 +1,24 @@
 package dao.interfaces;
 
+import model.Hobby;
+import model.Person;
+
+import java.util.List;
+
 public interface IHobbyDAO {
+
+    void createHobby(Hobby hobby);
+
+    Hobby updateHobby(Hobby hobby);
+
+    void deleteHobby(Hobby hobby);
+
+    Hobby findById(Integer hobbyId);
+
+    List<Hobby> findHobby();
+
+    List<Person> findPersonByHobby(Hobby hobby);
+
+    
+
 }
--- src/main/java/model/Hobby.java
+++ src/main/java/model/Hobby.java
@@ -11,7 +11,7 @@ public class Hobby {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false, unique = true)
-    private String id;
+    private Integer hobbyId;
 
     @Column(name = "name", nullable = false)
     private String name;
