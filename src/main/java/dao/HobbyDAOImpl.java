601b4ea2e8d41e503aea68ec67ee0f57d7751c88
ConfuzzedCat <73724128+ConfuzzedCat@users.noreply.github.com>
2023-09-06 12:38:44 +0200

    Us5 done

--- src/main/java/dao/HobbyDAOImpl.java
+++ src/main/java/dao/HobbyDAOImpl.java
@@ -3,6 +3,7 @@ package dao;
 import dao.interfaces.IHobbyDAO;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
 import model.Hobby;
 import model.Person;
@@ -90,8 +91,14 @@ public class HobbyDAOImpl implements IHobbyDAO {
     @Override
     public Map<String, Integer> findAmountOfUsersForeachHobby() {
         Map<String, Integer> hobbiesAndAmount = new HashMap<>();
+        List<Object[]> retValue;
         try (EntityManager em = emf.createEntityManager()) {
-
+            Query query = em.createNamedQuery("Hobby.findCountForAllHobbies", Hobby.class);
+            retValue = query.getResultList();
         }
+        for (Object[] elementValues : retValue) {
+            hobbiesAndAmount.put((String)elementValues[0], (Integer)elementValues[1]);
+        }
+        return hobbiesAndAmount;
     }
 }
--- src/main/java/model/Hobby.java
+++ src/main/java/model/Hobby.java
@@ -21,7 +21,7 @@ import java.util.Set;
                 //US 5, mangler count
                 @NamedQuery(name = "Hobby.deleteAllHobbies", query = "delete from Hobby h"),
                 @NamedQuery(name = "Hobby.findById", query = "select h from Hobby h where h.id = :id"),
-                @NamedQuery(name = "Hobby.findCountForAllHobbies", query = "SELECT h.name, COUNT(p) FROM Hobby h JOIN h.person p GROUP BY h.name"),
+                @NamedQuery(name = "Hobby.findCountForAllHobbies", query = "SELECT h.name, COUNT(p) FROM Hobby h JOIN h.persons p GROUP BY h.name"),
                 @NamedQuery(name = "Hobby.deleteFromId", query = "delete from Hobby h where h.id = :id")
         }
 )
